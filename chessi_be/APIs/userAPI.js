d30e3a803488d1227bce8b36b27e4b1529065c20
hwangahn <haha.in.advance@gmail.com>
2023-12-26 23:17:48 +0700

    - Added change password function - Added protected route for user

--- chessi_be/APIs/userAPI.js
+++ chessi_be/APIs/userAPI.js
@@ -1,11 +1,11 @@
 const express = require('express')
-const { handleGetUserData, handleUserFollow, handleUserUnfollow } = require('../controllers/userController');
+const { handleGetUserData, handleUserChangePassword, handleUserFollow, handleUserUnfollow } = require('../controllers/userController');
 const { verifyJWT, verifyAccessToken } = require('../middlewares/auth');
 
 let router = express.Router();
 
 router.get('/api/user/:userid', handleGetUserData);
-router.post('/api/user/follow/:userid', verifyJWT, verifyAccessToken, handleUserFollow); // userid indicates user to follow
-router.delete('/api/user/follow/:userid', verifyJWT, verifyAccessToken, handleUserUnfollow); // userid indicates user to follow
+router.post('/api/user/:userid/follow', verifyJWT, verifyAccessToken, handleUserFollow); // userid indicates user to follow
+router.delete('/api/user/:userid/follow/', verifyJWT, verifyAccessToken, handleUserUnfollow); // userid indicates user to follow
 
 module.exports = router;
